a551c76410aefa6ab594da637e6cfc96d9e679e7
Kyaw Swar Thant <hmm.aungthuheain@gmail.com>
2023-06-14 21:41:21 +0630

    initial

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card">
+    <div class="flex">
+      <div>
+        <h4 class="title" @click="detail = !detail">{{ project.title }}</h4>
+      </div>
+      <div>
+        <span class="material-icons"> task_alt </span>
+        <span class="material-icons">delete_forever</span>
+        <span class="material-icons">edit</span>
+      </div>
+    </div>
+
+    <p v-if="detail">{{ project.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      detail: false,
+    };
+  },
+};
+</script>
+
+<style>
+.card {
+  padding: 10px;
+  background: silver;
+  margin: 5px;
+  border-radius: 10px;
+  border-left: 5px solid crimson;
+}
+.title {
+  color: indigo;
+  cursor: pointer;
+}
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+span {
+  margin-left: 5px;
+  cursor: pointer;
+}
+span:hover {
+  color: blueviolet;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <singleProject :project="project" />
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from "@/components/SingleProject.vue";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: { SingleProject },
+  name: "HomeView",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.projects = data;
+      })
+      .catch(() => {});
+  },
+};
 </script>
